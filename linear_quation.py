2dde915e961cdeb07daa6ecccaff965272e3c5ec
Algin Joseph <algin1995@gmail.com>
2021-01-15 23:50:32 +0530

    Formatting Corrected

--- linear_quation.py
+++ linear_quation.py
@@ -5,8 +5,8 @@ from tensorflow.keras.layers import Dense
 model = Sequential([Dense(units=1, input_shape=[1])])
 model.compile(optimizer='sgd', loss='mean_squared_error')
 
-xs = np.array([-1,0,1,2,3,4],dtype=float)
-ys = np.array([-3,-1,1,3,5,7],dtype=float)
+xs = np.array([-1, 0, 1, 2, 3, 4], dtype=float)
+ys = np.array([-3, -1, 1, 3, 5, 7], dtype=float)
 
 # Fails without Normalization
 # xs = np.array([-8756923, 0, 1, 2, 3, 48526], dtype=float)
